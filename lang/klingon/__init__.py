8c0b284208f0480a25fa861f2e7cbc0614cd896c
ajanco <apjanco@gmail.com>
2019-12-24 10:02:14 -0500

    ready to begin custom stop words, and exceptions

--- lang/klingon/__init__.py
+++ lang/klingon/__init__.py
@@ -1,9 +1,20 @@
 # new language from scratch
+#from klingon.stop_words import STOP_WORDS
+#from klingon.tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+#from klingon.norm_exceptions import NORM_EXCEPTIONS
+#from klingon.lex_attrs import LEX_ATTRS
+from spacy.lang.tokenizer_exceptions import BASE_EXCEPTIONS
+from spacy.lang.norm_exceptions import BASE_NORMS
 from spacy.language import Language
+from spacy.attrs import LANG, NORM
+from spacy.util import update_exc, add_lookups
+
+
 
 # https://spacy.io/usage/adding-languages#language-subclass
 class KlingonDefaults(Language.Defaults):
-    pass
+    lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters[LANG] = lambda text: "klingon"
     '''lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: "custom_sr"
